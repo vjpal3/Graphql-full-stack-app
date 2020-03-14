3f82d70a7673d6020ef1d267a74da7fe26677813
vjpal3 <vrishalipal@yahoo.com>
2020-03-14 17:54:54 -0400

    Add front end with react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BookList from './components/BookList';
+
+class App extends Component {
+  render() {
+    return (
+      <div id='main'>
+        <h1>My Reading List</h1>
+        <BookList />
+      </div>
+    );
+  }
+}
+
+export default App;
