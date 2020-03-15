83bd6b30723c92d8881659bb6379010feaddb34c
vjpal3 <vrishalipal@yahoo.com>
2020-03-15 19:21:30 -0400

    Add BookDetails component and fetch book-details from GraphQL Server

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 
 import { getBooksQuery } from '../queries/queries';
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
+
   displayBooks() {
     let data = this.props.data;
     if (data.loading) {
       return <div>Loading books...</div>;
     } else {
       return data.books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={e => {
+              this.setState({ selected: book.id });
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   }
+
   render() {
     return (
       <div>
-        <ul id='book-list'>{this.displayBooks()}</ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
